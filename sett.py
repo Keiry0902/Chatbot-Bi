9dedd0903e08a20c2f218ad98813c872f81f9490
Keiry0902 <71118003+Keiry0902@users.noreply.github.com>
2024-08-12 22:14:22 -0500

    Add files via upload

---
+++ sett.py
@@ -0,0 +1,22 @@
+token= 'bigdateros'
+
+whatsapp_token = 'EAAQ1aVQhmgQBO0zU5ecpEITjke9EF4zluWLU2PNxfiF6b6LuV0LIktGhqosz51iw1m7HpZCW2IjzWUn2hTV4brzt7msOIK6EX4582afYcx1Jd0rHOGp7TIwdKHoIfAStICRnGHi7ouWlP4VZARR483YG0YP22Sf99GZBf6HXQpaZB4cxoJgkTaSvxn8NqP9HqDTMiecW9ofYs3bZApQZDZD'
+whatsapp_url = 'https://graph.facebook.com/v20.0/358434264028149/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
